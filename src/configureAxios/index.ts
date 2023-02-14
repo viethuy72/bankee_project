2a6c93f403be021dfada4fc4cbcb9ee60bbaf0f0
mickey-ichi <mickey@react-plus.com>
2023-02-14 14:44:10 +0700

    add example get token add to header request axios

--- src/configureAxios/index.ts
+++ src/configureAxios/index.ts
@@ -4,4 +4,21 @@ export function configureAxios() {
   if (process.env.REACT_APP_API_URL) {
     axios.defaults.baseURL = process.env.REACT_APP_API_URL
   }
+  // Add a request interceptor
+  axios.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      config.headers = config.headers ?? {}
+      // Example Get token from store and add to header for each request
+      // let authToken = store.getState().session.token
+      // if (authToken) {
+      //   config.headers['Authorization'] = `Bearer ${authToken}`
+      // }
+      return config
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error)
+    },
+  )
 }
